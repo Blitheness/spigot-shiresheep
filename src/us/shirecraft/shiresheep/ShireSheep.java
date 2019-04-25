213786a6f7f26cef0d4420601d13d1879aaa8487
Andy <blitheness@oneiric.co.uk>
2019-04-26 00:24:56 +0100

    Read config on enable and save on disable

--- src/us/shirecraft/shiresheep/ShireSheep.java
+++ src/us/shirecraft/shiresheep/ShireSheep.java
@@ -1,16 +1,28 @@
 package us.shirecraft.shiresheep;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ShireSheep extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
-		
+		loadConfiguration();
 	}
 	
 	@Override
 	public void onDisable() {
-  
+		saveConfiguration();
 	}
+	
+	public void loadConfiguration() {
+		saveDefaultConfig();
+		config = getConfig();
+	}
+	
+	public void saveConfiguration() {
+		saveConfig();
+	}
+
+	FileConfiguration config;
 }
