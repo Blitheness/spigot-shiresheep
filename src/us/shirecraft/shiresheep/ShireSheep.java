850e6de139177f2fbb5fe10e58c92aacb9de20fc
Andy <blitheness@oneiric.co.uk>
2019-04-26 00:22:09 +0100

    Add main class extending JavaPlugin

---
+++ src/us/shirecraft/shiresheep/ShireSheep.java
@@ -0,0 +1,16 @@
+package us.shirecraft.shiresheep;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ShireSheep extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+  
+	}
+}
